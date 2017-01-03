95955a52234862ea6a4744a16d404ae1c9f9b573
Jody <wolaver.d@gmail.com>
2017-01-02 22:38:56 -0500

    file added
    setup now written in python

---
+++ setup.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+import sys
+
+def link_files(src, dest):
+	if os.path.exists(dest):
+		os.unlink(dest)
+
+	pdir = os.path.dirname(dest)
+
+	if not os.path.isdir(pdir):
+		os.mkdir(dest_pdir)
+
+	os.symlink(src, dest)
+
+
+home = os.path.expanduser('~')
+repo = os.getcwd()
+
+dotfiles = subprocess.getoutput('git ls-files').splitlines()
+
+for dotfile in dotfiles:
+	if dotfile != 'setup.py' and dotfile != '.gitignore':
+		spath = os.path.abspath(dotfile)
+		dpath = os.path.join(home, dotfile)
+
+		link_files(spath, dpath)
