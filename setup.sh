a371b77a886de5bea9329fa136dd2719cb99cc82
Jody <wolaver.d@gmail.com>
2017-01-24 15:34:10 -0500

    wip

--- setup.sh
+++ setup.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
 declare -a dotfiles
+
+case "$SHELL" in
+	*bash)
+		inc_dir='.bash'
+		;;
+	*zsh)
+		inc_dir='.zsh'
+		;;
+	*)
+		exit 1
+		;;
+esac
+
 dotfiles=(
-	'./.bash/aliases'
-	'./.bash/env'
-	'./.bash/functions'
-	'./.bash/prompt'
-	'./.bash_profile'
-	'./.bashrc'
-	'./.config/youtube-dl/youtube-dl.conf'
-	'./.ssh/config'
-	'./.vim/vimrc'
+	"./${inc_dir}"
+	'./functions'
+	'./aliases'
+	'./.vim'
 )
 
 for dotfile in "${dotfiles[@]}"; do
+	# Abspath to repo's copy.
 	src=$(realpath "$dotfile")
+	# Substitute dotslash for $HOME.
 	dest="${dotfile/.\//$HOME/}"
 	dest_pdir=$(dirname "$dest")
 
-	[[ -e $dest || -h $dest ]] && rm -f "$dest"
-	[[ ! -d $dest_pdir && -w $(dirname "$dest_pdir") ]] && mkdir -p "$dest_pdir"
+	# Removes any conflicting files or links.
+	[[ -e $dest || -h $dest ]] && rm -rf "$dest"
+	if [[ ! -d $dest_pdir && -w $(dirname "$dest_pdir") ]]; then
+		mkdir -p "$dest_pdir"
+	fi
 
 	ln -s "$src" "$dest" || break
-
-	[[ $dotfile = './.ssh/config' ]] && chmod 600 "$dotfile"
 done
